cbd53bb708074ace211f8b1f8e5ffa1df6b7b8e4
kevinAntony034 <kevinag034@gmail.com>
2024-11-10 20:04:29 +0100

    Add requirements.txt and setup.py

--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -1,7 +1,5 @@
 import unittest
 from math_quiz import Limitfunc, Operatorfunc, Mathfunc
-
-
 class TestMathGame(unittest.TestCase):
 
     def test_Limitfunc(self):
@@ -11,29 +9,24 @@ class TestMathGame(unittest.TestCase):
         for _ in range(1000):  # Test a large number of random values
             rand_num = Limitfunc(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
-
     def test_Operatorfunc(self): 
         valid = ("+  -  *")
         for _ in range(1000):
                 operator = Operatorfunc()
                 self.assertIn(operator,valid)
         pass  
-
     def test_Mathfunc(self):
             test_case = [
                 (5, 2, '+', '5 + 2', 7),
                 (6, 1, '-', '6 - 1', 5),
                 (3, 3, '*', '3 * 3', 9),
                 (4, 4, '+', '4 + 4', 8),
-                (9, 5, '-', '9 - 5', 4),
-               
+                (9, 5, '-', '9 - 5', 4),         
             ]
-
             for number1, number2, operator, expected_problem, expected_answer in test_case:
                 prob, answer = Mathfunc(number1, number2, operator)
-                self.assertEqual(prob, expected_problem)  
+                self.assertEqual(prob, expected_problem)   
                 self.assertEqual(answer, expected_answer)
             pass
-
 if __name__ == "__main__":
     unittest.main()
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name="math_quiz",  # The name of your package
+    version="0.1",     # Package version
+    packages=["math_quiz"],  # List your package(s) here
+)
